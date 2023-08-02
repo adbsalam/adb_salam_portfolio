e7b46465586c8b635c576edfe365212fb2f06dad
Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>
2023-08-02 23:16:35 +0100

    updated spending

--- info/feature/src/main/java/uk/adbsalam/portfolio/info/feature/InfoGraphics.kt
+++ info/feature/src/main/java/uk/adbsalam/portfolio/info/feature/InfoGraphics.kt
@@ -1,6 +1,8 @@
 package uk.adbsalam.portfolio.info.feature
 
 import android.content.res.Configuration
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -16,10 +18,26 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.patrykandpatrick.vico.compose.axis.horizontal.bottomAxis
+import com.patrykandpatrick.vico.compose.axis.vertical.startAxis
+import com.patrykandpatrick.vico.compose.chart.Chart
+import com.patrykandpatrick.vico.compose.chart.column.columnChart
+import com.patrykandpatrick.vico.compose.chart.line.lineChart
+import com.patrykandpatrick.vico.core.axis.AxisPosition
+import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
+import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
+import com.patrykandpatrick.vico.core.entry.FloatEntry
+import com.patrykandpatrick.vico.core.entry.entryModelOf
+import com.patrykandpatrick.vico.core.entry.entryOf
+import com.patrykandpatrick.vico.core.formatter.DecimalFormatValueFormatter
+import com.patrykandpatrick.vico.core.formatter.ValueFormatter
 import uk.adbsalam.portfolio.info.feature.components.infocards.AndroidMainCard
 import uk.adbsalam.portfolio.info.feature.components.infocards.SkillsInsightCard
 import uk.adbsalam.portfolio.theming.Adb_Theme
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun InfoGraphics() {
 
@@ -40,11 +58,53 @@ fun InfoGraphics() {
         )
 
         SkillsInsightCard()
-        Spacer(modifier = Modifier.height(30.dp))
 
+        Spacer(modifier = Modifier.height(10.dp))
+
+        val data = listOf(
+            "2022-07-01" to 2f,
+            "2022-07-02" to 6f,
+            "2022-07-04" to 4f
+        ).associate { (dateString, yValue) ->
+            LocalDate.parse(dateString) to yValue
+        }
+
+        val chartEntryModelProducer = ChartEntryModelProducer(
+            listOf(
+                FloatEntry(0f, y = 50f),
+                FloatEntry(1f, y = 40f),
+                FloatEntry(2f, y = 60f),
+                FloatEntry(3f, y = 90f),
+            )
+        )
+
+        val xValuesToDates = data.keys.associateBy { it.toEpochDay().toFloat() }
+        val chartEntryModel =
+            xValuesToDates.keys.zip(data.values) { x, y -> entryOf(x, y) }.let { entryModelOf(it) }
+        val horizontalAxisValueFormatter = AxisValueFormatter<AxisPosition.Horizontal.Bottom> { value, _ ->
+            (xValuesToDates[value]
+                ?: LocalDate.ofEpochDay(value.toLong())).format(DateTimeFormatter.ofPattern("dd MMM"))
+        }
+
+        Chart(
+            chart = lineChart(),
+            model = chartEntryModel
+        )
+
+        Chart(
+            chart = lineChart(),
+            chartModelProducer = chartEntryModelProducer,
+            startAxis = startAxis(),
+            bottomAxis = bottomAxis(
+                valueFormatter = horizontalAxisValueFormatter
+            ),
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview
 @Composable
 fun InfoGraphicsPreviewLight() {
@@ -53,6 +113,7 @@ fun InfoGraphicsPreviewLight() {
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun InfoGraphicsPreviewDark() {
