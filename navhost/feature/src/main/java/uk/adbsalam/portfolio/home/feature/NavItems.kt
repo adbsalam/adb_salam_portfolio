f3123496d1fd1a107b3f05ca1bea8803e5a43e0c
Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>
2023-08-10 00:47:55 +0100

    nav host setup

---
+++ navhost/feature/src/main/java/uk/adbsalam/portfolio/home/feature/NavItems.kt
@@ -0,0 +1,38 @@
+package uk.adbsalam.portfolio.home.feature
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Forum
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Movie
+import androidx.compose.ui.graphics.vector.ImageVector
+
+
+data class NavItem(
+    val title: String,
+    val icon: ImageVector,
+    val index: Int
+)
+
+val navItems = listOf(
+    NavItem(
+        title = "Home",
+        icon = Icons.Outlined.Home,
+        index = 0
+    ),
+    NavItem(
+        title = "Info",
+        icon = Icons.Outlined.Info,
+        index = 0
+    ),
+    NavItem(
+        title = "Videos",
+        icon = Icons.Outlined.Movie,
+        index = 0
+    ),
+    NavItem(
+        title = "Reviews",
+        icon = Icons.Outlined.Forum,
+        index = 0
+    ),
+)
---
+++ navigation/src/main/java/uk/adbsalam/portfolio/navigation/AppNavigator.kt
@@ -0,0 +1,31 @@
+package uk.adbsalam.portfolio.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+
+private fun NavController.rootNavOptions() = NavOptions
+    .Builder()
+    .setPopUpTo(
+        this.graph.startDestinationId,
+        inclusive = false,
+        saveState = true
+    )
+    .setLaunchSingleTop(true)
+    .setRestoreState(true)
+    .build()
+
+fun NavController.navigateToHome() {
+    this.navigate(NavigationScreen.OnHome.route(), this.rootNavOptions())
+}
+
+fun NavController.navigateToInfo() {
+    this.navigate(NavigationScreen.OnInfo.route(), this.rootNavOptions())
+}
+
+fun NavController.navigateToVideos() {
+    this.navigate(NavigationScreen.OnVideos.route(), this.rootNavOptions())
+}
+
+fun NavController.navigateToReviews() {
+    this.navigate(NavigationScreen.OnReviews.route(), this.rootNavOptions())
+}
