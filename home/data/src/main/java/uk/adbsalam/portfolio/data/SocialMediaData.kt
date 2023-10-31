5c1cd5c8211cc8ea8e9bb7fffcf9e87aed417b4b
adb_salam <39949965+MuhammadAbdulSalam@users.noreply.github.com>
2023-10-31 18:02:54 +0000

    release: 1.2 social media links updated
    release: 1.2 social media links updated

--- home/data/src/main/java/uk/adbsalam/portfolio/data/SocialMediaData.kt
+++ home/data/src/main/java/uk/adbsalam/portfolio/data/SocialMediaData.kt
@@ -4,7 +4,7 @@ package uk.adbsalam.portfolio.data
  * Home Social Media links to be used
  */
 enum class SocialMedia(val link: String) {
-    G_PLAY(""),
+    G_PLAY("https://play.google.com/store/apps/details?id=uk.adbsalam.portfolio&pcampaignid=web_share"),
     LINKED_IN("https://www.linkedin.com/in/muhammad-abdulsalam-1253a7178/"),
     GIT("https://github.com/MuhammadAbdulSalam"),
     YOUTUBE("https://www.youtube.com/channel/UCct4uE53LK-r_0DlNBM_InA"),
--- videos/data/src/main/java/uk/adbsalam/portfolio/videos/data/objects/VideoItems.kt
+++ videos/data/src/main/java/uk/adbsalam/portfolio/videos/data/objects/VideoItems.kt
@@ -18,23 +18,23 @@ data class VideoItems(
         fun createMock() = VideoItems(
             listOf(
                 Video(
-                    title = "Test Title",
-                    description = "lorum ipsum somehting to show as description",
+                    title = "Switzerland travel diary",
+                    description = "Exploring some of the most beautiful places in Switzerland. Capturing most amazing views and location, if you would like to explore Switzerland someday, this video is for you",
                     videoId = "DE5o92ya25I"
                 ),
                 Video(
-                    title = "Test Title",
-                    description = "lorum ipsum somehting to show as description",
+                    title = "Croatia - Game of Thrones trip!",
+                    description = "Trip to Croatia from Splits to Dubrovnik in search for all shooting locations of game of thrones. Feast your eyes with some stunning views from Croatia",
                     videoId = "OFqTWt4OnLE"
                 ),
                 Video(
-                    title = "Test Title",
-                    description = "lorum ipsum somehting to show as description",
+                    title = "Snowdonia - Day Ride",
+                    description = "Having a day trip for bike ride across Snowdonia. A hike to Jublee tower, a drive to Penn Y Pass and beautiful views of Swallow Falls",
                     videoId = "MS-CiBqGpbg"
                 ),
                 Video(
-                    title = "Test Title",
-                    description = "lorum ipsum somehting to show as description",
+                    title = "Bamford's Edge",
+                    description = "If you love Motorcycle rides, this route will be one of the best to explore in UK. Lovely ride from SnakePass that ends with an amazing hike to Bamford's edge.",
                     videoId = "rYc5O8mSuF4"
                 )
             )
